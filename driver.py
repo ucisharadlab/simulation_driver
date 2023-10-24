ef8e5ab082b9523b9d6ddbde298671f936413e87
Sriram Rao <sriramrao@dhcp-10-8-019-233.mobile.reshsg.uci.edu>
2023-10-24 10:40:58 -0700

    Code for threaded execution

--- driver.py
+++ driver.py
@@ -1,48 +1,61 @@
 import json
+import threading
 import time
 from datetime import datetime
 
 from plan.planner import Planner, get_planner
 from repo.edb_repo import EdbRepo
-from simulator.simulator import Simulator, get_simulator
-from util import reflection_util
+from simulator.simulator import get_simulator
 
 
 class Driver:
     def __init__(self, simulator_repo, sleep_seconds: int = 0):
         self.repo = simulator_repo
         self.planners = dict()
-        self.threads = dict()
+        self.threads = list()
         self.sleep_seconds = sleep_seconds
 
     def run(self):
         while True:
             query_load = bundle(self.repo.get_query_load())
-            for learn_query in query_load["learn"]:
-                _, simulator_name, planner_name, test_table = learn_query["query"].split(":")
-                self.set_planner(simulator_name, planner_name, self.repo.get_test_data(test_table))
-            for query in query_load["data"]:
-                parsed_query = parse_query(query, self.repo)
-                simulator_details = self.repo.get_simulator(query["output_type"])
-                if len(simulator_details.keys()) <= 0:
-                    continue
-                simulator_name = simulator_details["name"]
-                execution_info = self.plan_and_simulate(parsed_query, simulator_name)
-                self.repo.log(simulator_name, execution_info)
+            self.handle_learn_queries(query_load["learn"])
+            self.handle_data_queries(query_load["data"])
+            self.check_simulation_statuses()
             print("Finished cycle")
             time.sleep(self.sleep_seconds)
 
-    def plan_and_simulate(self, query: dict, simulator_name: str):
+    def handle_learn_queries(self, learn_queries: list):
+        for query in learn_queries:
+            _, simulator_name, planner_name, test_table = query["query"].split(":")
+            self.set_planner(simulator_name, planner_name, self.repo.get_test_data(test_table))
+
+    def handle_data_queries(self, data_queries: list):
+        for query in data_queries:
+            parsed_query = parse_query(query, self.repo)
+            simulator_details = self.repo.get_simulator(query["output_type"])
+            if len(simulator_details.keys()) <= 0:
+                continue
+            simulator_name = simulator_details["name"]
+            previous_runs = self.repo.get_log(simulator_name)
+            plan = self.planners[simulator_name].get_plan(previous_runs, query)
+            for params in plan:
+                thread = threading.Thread(target=self.run_simulation, args=(parsed_query, simulator_name, params))
+                self.threads.append(thread)
+                thread.start()
+
+    def check_simulation_statuses(self):
+        for t in self.threads:
+            if not t.is_alive():
+                t.handled = True
+        self.threads = [t for t in self.threads if not t.handled]
+
+    def run_simulation(self, query: dict, simulator_name: str, params: dict):
         print(f"Query: {query['id']}, beginning plan and simulate")
         print("Planning simulator input")
-        previous_runs = self.repo.get_log(simulator_name)
-        choice = self.planners[simulator_name].get_best_choice(previous_runs, query)
-        if choice is None:
-            return dict()
-
         execution_info = dict()
-        execution_info["params"] = json.loads(list(choice.keys())[0])
+        execution_info["params"] = params
         simulator = get_simulator(simulator_name)
+
         print("Running simulation")
         start = datetime.now()
         simulator.run(execution_info["params"])
@@ -51,7 +64,7 @@ class Driver:
 
         print("Storing projected outputs")
         self.repo.store_result(query["output_type"], results)
-        return execution_info
+        self.repo.log(simulator_name, execution_info)
 
     def set_planner(self, simulator_name: str, planner_name: str, test_data: dict = None) -> Planner:
         planner = get_planner(planner_name)
@@ -83,8 +96,10 @@ def parse_query(query: dict, repo: EdbRepo) -> dict:
         check_query = (f"SELECT column_name, type_key, data_type FROM simulated_columns "
                        f"WHERE table_name = '{from_query}' AND column_name = '{column}'")
         result = repo.fetch_entity(check_query)
-        if result: break
-    if not result: return query
+        if result:
+            break
+    if not result:
+        return query
     query["simulated_column"] = result[0]
     query["output_type"] = result[2]
     query["join_key"] = result[1]
--- plan/planner.py
+++ plan/planner.py
@@ -29,12 +29,26 @@ class GreedyPlanner(Planner):
         parameter_choices = self.estimator.dump_model()
         ordered_parameters = sorted(parameter_choices.items(), key=lambda e: e[1]["cost"])
         previous_params = [entry["params"] for entry in log]
-        available_params = list()
-        for param, cost in ordered_parameters:
+        chosen_params = self.choose_parameters(ordered_parameters)
+
+        plan = list()
+        for param, cost in chosen_params:
             if json.loads(param) not in previous_params:
-                available_params.append({param: cost})
-        # ordered_parameters = [param for param in ordered_parameters if param not in previous_params]
-        return available_params
+                plan.append({param: cost})
+        return plan
+
+    def choose_parameters(self, ordered_parameters: list):
+        plan = list()
+        if len(ordered_parameters) == 0:
+            return plan
+        plan.append(ordered_parameters[0])
+        if len(ordered_parameters) == 1:
+            return plan
+        plan.append(ordered_parameters[1])
+        if len(ordered_parameters) == 2:
+            return plan
+        plan.insert(0, ordered_parameters[-1])
+        return plan
 
 
 def get_planner(name: str) -> Planner:
