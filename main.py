3975758bd8a671fbb8de964be2a9c7f84e3a9861
Sriram Rao <sriramrao@dhcp-10-8-019-233.mobile.reshsg.uci.edu>
2023-11-29 15:06:40 -0800

    Adding base path as command line arg

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def quality_check():
     parser.add_argument("-d", "--date", required=True, help="Datetime (yyyy-MM-dd_HH-mm) when the test was run.")
     parser.add_argument("-r", "--runid", required=True, help="Run ID of the test that should be considered ground truth")
     parser.add_argument("-t", "--threadname", default="", help="Name of the thread that ran the test.")
+    parser.add_argument("-b", "--basepath", default="./debug/hysplit_out", help="Base directory of the test.")
     args = parser.parse_args()
     defaults = {"%output_grids%::%spacing%": "0.001 0.001",
                 "%output_grids%::%sampling%": "00 00 05",
@@ -29,7 +30,8 @@ def quality_check():
     logger.info(f"Name: {args.name}, Date: {args.date}, Run ID: {args.runid}")
     quality.measure_quality(
         test_details={"name": args.name, "date": args.date, "thread_name": args.threadname, "params": defaults},
-        base_details={"name": args.name, "date": args.date, "thread_name": args.threadname, "params": defaults, "run_id": args.runid})
+        base_details={"name": args.name, "date": args.date, "thread_name": args.threadname, "params": defaults, "run_id": args.runid},
+        base_path=args.basepath)
 
 
 if __name__ == '__main__':
